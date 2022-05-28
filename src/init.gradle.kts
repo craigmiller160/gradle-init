e3ca98b5b5beb4b5a57049b5bb6db8b5b491a95d
Craig Miller <craigmiller160@gmail.com>
2022-05-28 15:39:39 -0400

    Bumped plugin version

--- src/init.gradle.kts
+++ src/init.gradle.kts
@@ -4,7 +4,7 @@ import java.nio.file.StandardCopyOption
 import io.craigmiller160.craigbuild.gradle.plugin.CraigBuildGradlePlugin
 
 initscript {
-    val craigBuildGradlePluginVersion = "1.0.0-SNAPSHOT"
+    val craigBuildGradlePluginVersion = "1.0.0"
 
     repositories {
         mavenCentral()
