ebbc9771400c5c1f44015c4a45fcf3a392da651c
Craig Miller <craigmiller160@gmail.com>
2022-05-28 15:38:58 -0400

    Cleaning up init script

--- src/init.gradle.kts
+++ src/init.gradle.kts
@@ -1,9 +1,28 @@
 import java.nio.file.Files
 import java.nio.file.Paths
 import java.nio.file.StandardCopyOption
+import io.craigmiller160.craigbuild.gradle.plugin.CraigBuildGradlePlugin
+
+initscript {
+    val craigBuildGradlePluginVersion = "1.0.0-SNAPSHOT"
+
+    repositories {
+        mavenCentral()
+        maven {
+            url = uri("https://repo.gradle.org/gradle/libs-releases")
+        }
+        maven {
+            url = uri("https://craigmiller160.ddns.net:30003/repository/maven-public")
+        }
+    }
+
+    dependencies {
+        classpath("io.craigmiller160:craig-build-gradle-plugin:$craigBuildGradlePluginVersion")
+    }
+}
 
 allprojects {
-    task("installDefaultGitHooks") {
+    tasks.register("installDefaultGitHooks") {
         val hooksSourceDir = Paths.get(System.getProperty("user.home"), ".gradle", "gitHooks")
         val hooksTargetDir = Paths.get(rootProject.rootDir.absolutePath, ".git", "hooks")
         runCatching {
@@ -14,4 +33,6 @@ allprojects {
                 }
         }
     }
+
+    apply<CraigBuildGradlePlugin>()
 }
