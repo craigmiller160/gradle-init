e60249d947f96754a33109043dcea4c1efc4130b
Craig Miller <craigmiller160@gmail.com>
2023-03-05 14:09:17 -0500

    Cleanup

--- src/init.gradle.kts
+++ src/init.gradle.kts
@@ -30,19 +30,19 @@ allprojects {
         }
     }
 
-    tasks.register("installDefaultGitHooks") {
-        println("Installing Default Git Hooks")
-        val hooksSourceDir = Paths.get(System.getProperty("user.home"), ".gradle", "gitHooks")
-        val hooksTargetDir = Paths.get(rootProject.rootDir.absolutePath, ".git", "hooks")
-        runCatching {
-            Files.list(hooksSourceDir)
-                .forEach { hookPath ->
-                    val hookGitPath = hooksTargetDir.resolve(hookPath.fileName)
-                    Files.copy(hookPath, hookGitPath, StandardCopyOption.REPLACE_EXISTING, StandardCopyOption.COPY_ATTRIBUTES)
-                }
-        }
-            .onFailure { it.printStackTrace() }
-    }
+//    tasks.register("installDefaultGitHooks") {
+//        println("Installing Default Git Hooks")
+//        val hooksSourceDir = Paths.get(System.getProperty("user.home"), ".gradle", "gitHooks")
+//        val hooksTargetDir = Paths.get(rootProject.rootDir.absolutePath, ".git", "hooks")
+//        runCatching {
+//            Files.list(hooksSourceDir)
+//                .forEach { hookPath ->
+//                    val hookGitPath = hooksTargetDir.resolve(hookPath.fileName)
+//                    Files.copy(hookPath, hookGitPath, StandardCopyOption.REPLACE_EXISTING, StandardCopyOption.COPY_ATTRIBUTES)
+//                }
+//        }
+//            .onFailure { it.printStackTrace() }
+//    }
 
     apply<CraigBuildGradlePlugin>()
 }
